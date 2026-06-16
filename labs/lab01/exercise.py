f1918c58096e7916f1a2368748453e2ecf835e00
Joycebhvn <j.beauhavan@gmail.com>
2026-06-16 19:11:17 +0800

    Change the print in Lab 1

--- labs/lab01/exercise.py
+++ labs/lab01/exercise.py
@@ -1,2 +1,4 @@
 print("Hello everyone, Lab 01")
 print("This is the first exercise of the lab.")
+print("In this exercise, we will learn how to print messages in Python.")
+print("You can use the print() function to display messages on the screen.")
