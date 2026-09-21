00236afa01155a1d191c57888f4bef99959a312f
zana080 <aguskoplak1998@gmail.com>
2026-09-21 08:26:50 +0800

    lab 10 exercise 1

--- labs/lab10/exercise1/exercise1.py
+++ labs/lab10/exercise1/exercise1.py
@@ -1,6 +1,15 @@
-num_rounds = int(input())
-for 
+num_rounds = int(input("Number of rounds: "))
+total_score = 0.0
+for i in range (num_rounds):
+    score =  int (input ("score: "))
+    if score > 100:
+        score = (score * 0.2) + score
+        total_score = total_score + score
+    else:
+        total_score = total_score + score
 
+final_score = total_score
+rounds_processed = num_rounds
 
 print(f"{final_score:.1f}")
 print(rounds_processed)
