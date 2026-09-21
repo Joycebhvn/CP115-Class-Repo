313fd3b1e596a5703b1f916570b4ad0f5385faae
zana080 <aguskoplak1998@gmail.com>
2026-09-21 08:49:00 +0800

    lab 10 exercise 2

--- labs/lab10/exercise2/exercise2.py
+++ labs/lab10/exercise2/exercise2.py
@@ -1,6 +1,14 @@
 num_days = int(input())
 danger_threshold = float(input())
-
+average_temp = 0.0
+total_temp = 0.0
+danger_days = 0
+for day in range (num_days):
+    temp = float(input("Temperature: "))
+    if temp > danger_threshold:
+        danger_days = danger_days + 1
+    total_temp = total_temp + temp
+average_temp = total_temp / num_days
 
 
 print(danger_days)
